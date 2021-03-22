b22add613c2b5827971bb45c3c8220bb45222834
Sumanth <48374330+tillu1208@users.noreply.github.com>
2021-03-22 16:42:32 +0530

    added add function

--- helloworld.java
+++ helloworld.java
@@ -3,4 +3,7 @@ class HelloWorld{
   public static void main(String []args){
     System.out.println("Hello World");
   }
+ public static void add(int num1,int num2){
+  System.out.println(num1+num2);
+ }
 }
